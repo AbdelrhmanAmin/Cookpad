191ba231a1baaf19a27e3042d1fb5cf2d38f9ec1
Abdo Amin <37663043+AbdelrhmanAmin@users.noreply.github.com>
2018-09-24 17:52:04 +0200

    pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
